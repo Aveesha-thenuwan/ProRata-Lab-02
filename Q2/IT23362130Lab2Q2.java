ea4103e9f3aaae1ead1d22957218a33a5fb1b16e
Aveesha-thenuwan <it23362130@my.sliit.lk>
2026-09-17 12:44:57 +0530

    Add Lab2Q2.java to Q2

---
+++ Q2/IT23362130Lab2Q2.java
@@ -0,0 +1,14 @@
+public class IT23362130Lab2Q2 {
+
+    public static void main(String[] args) {
+
+        double side = 10;
+        double PI = 3.14;
+
+        double perimeterOfSquare = 4 * side;
+        double radius = perimeterOfSquare / (2 * PI);
+
+        System.out.println("Radius of the circular fence: " + radius);
+
+    }
+}
